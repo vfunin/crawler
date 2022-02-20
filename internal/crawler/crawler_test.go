6e9d26d9924a6dc2f156b7eebc338fd34e237841
vfunin <i@vfunin.ru>
2022-02-20 14:01:26 +0300

    add crawler crawl test

--- internal/crawler/crawler_test.go
+++ internal/crawler/crawler_test.go
@@ -1,9 +1,16 @@
 package crawler
 
 import (
+	"context"
+	"html/template"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
+	"github.com/vfunin/crawler/internal/config"
 )
 
 func TestNew(t *testing.T) {
@@ -12,18 +19,78 @@ func TestNew(t *testing.T) {
 	assert.Equal(t, c.GetCnt(), int64(1))
 }
 
-func Test_crawler_Crawl(t *testing.T) {
+func TestCrawl(t *testing.T) {
+	log.Level(zerolog.TraceLevel)
+	cwv := context.WithValue(context.Background(), config.LoggerCtxKey, log.Logger)
+	ctx, cancel := context.WithCancel(cwv)
 
+	defer cancel()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		type tData struct {
+			Title string
+		}
+
+		tmpl, err := template.ParseFiles("../../mocks/crawler_crawl.html")
+		assert.Nil(t, err)
+		err = tmpl.Execute(w, tData{
+			Title: "Home page",
+		})
+		assert.Nil(t, err)
+	})
+
+	addr := "localhost:8080"
+	url := "http://" + addr + "/"
+	server := &http.Server{Addr: addr, Handler: nil}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			return
+		}
+	}()
+
+	time.Sleep(time.Second)
+
+	c := New(0, 0)
+
+	errCh := make(chan error)
+
+	go c.Crawl(ctx, cancel, url, false, 0, errCh)
+
+LOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			cancel()
+		case err := <-errCh:
+			assert.Nil(t, err)
+		case res := <-c.ResultCh():
+			assert.Equal(t, Result{
+				URL:   "http://localhost:8080/",
+				Title: "Home page",
+			}, res)
+		default:
+			if c.GetCnt() != 0 {
+				break
+			}
+
+			break LOOP
+		}
+	}
+
+	err := server.Shutdown(ctx)
+	assert.Nil(t, err)
 }
 
-func Test_crawler_DecCnt(t *testing.T) {
+func TestDecCnt(t *testing.T) {
 	c := New(0, 0)
 	assert.Equal(t, c.GetCnt(), int64(1))
 	c.DecCnt()
 	assert.Equal(t, c.GetCnt(), int64(0))
 }
 
-func Test_crawler_GetCnt(t *testing.T) {
+func TestGetCnt(t *testing.T) {
 	c := New(0, 0)
 	assert.Equal(t, c.GetCnt(), int64(1))
 	c.DecCnt()
@@ -32,21 +99,21 @@ func Test_crawler_GetCnt(t *testing.T) {
 	assert.Equal(t, c.GetCnt(), int64(1))
 }
 
-func Test_crawler_IncCnt(t *testing.T) {
+func TestIncCnt(t *testing.T) {
 	c := New(0, 0)
 	assert.Equal(t, c.GetCnt(), int64(1))
 	c.IncCnt()
 	assert.Equal(t, c.GetCnt(), int64(2))
 }
 
-func Test_crawler_IncMaxDepth(t *testing.T) {
+func TestIncMaxDepth(t *testing.T) {
 	c := New(0, 0)
 	assert.Equal(t, c.MaxDepth(), uint64(0))
 	c.IncMaxDepth(uint64(1))
 	assert.Equal(t, c.MaxDepth(), uint64(1))
 }
 
-func Test_crawler_ResultCh(t *testing.T) {
+func TestResultCh(t *testing.T) {
 	c := New(0, 0)
 	assert.NotEqual(t, c.ResultCh(), make(<-chan Result))
 }
